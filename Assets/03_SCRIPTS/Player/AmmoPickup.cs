95626c7dc4e46d35d391fe6b4bc81d40617e6ba8
Galalendon <galalendon@gmail.com>
2024-01-25 21:45:15 -0800

    Ammo Pickup Juice

--- Assets/03_SCRIPTS/Player/AmmoPickup.cs
+++ Assets/03_SCRIPTS/Player/AmmoPickup.cs
@@ -5,15 +5,61 @@ public class AmmoPickup : MonoBehaviour
     [SerializeField] float _ammoToPickup;
     [SerializeField] string _playerTag;
 
-    SphereCollider _collider;    
+    [Header("Bobbing")]
+    [SerializeField] GameObject _sprite;
+    [SerializeField] Transform _bobCeiling;
+    [SerializeField] Transform _bobFloor;
+    [SerializeField] float _bobSpeed;
+    [SerializeField] float _delay;
+
+    [Header("Sound")]
+    [SerializeField] AudioClip _pickupSound;
+
+    float _delayTimer = 0.0f;
+    Transform _target;
+    SphereCollider _collider;
+    bool _isWaiting = false;   
+    
     private void Start()
     {
         _collider = GetComponent<SphereCollider>();
+        _sprite.transform.position = _bobCeiling.position;
+        _target = _bobFloor;       
     }
+
+    private void Update()
+    {
+        if (_isWaiting)
+        {            
+            _delayTimer -= Time.deltaTime;
+            if (_delayTimer <= 0)
+            {
+                _isWaiting = false;                
+                _target = (_target == _bobCeiling) ? _bobFloor : _bobCeiling;
+            }
+        }
+        else
+        {
+            MoveTowardsTarget();
+        }
+    }
+
+    private void MoveTowardsTarget()
+    {        
+        float step = _bobSpeed * Time.deltaTime;
+        _sprite.transform.position = Vector3.MoveTowards(_sprite.transform.position, _target.transform.position, step);
+
+        if (_sprite.transform.position == _target.position)
+        {
+            _isWaiting = true;
+            _delayTimer = _delay;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {        
         if (other.CompareTag(_playerTag))
-        {
+        {            
             if (other.TryGetComponent<Ammo>(out var _playerAmmo))
             {
                 _playerAmmo.AmmoPickup(_ammoToPickup);
@@ -21,7 +67,7 @@ public class AmmoPickup : MonoBehaviour
                 {
                     _playerAmmo.Reload();
                 }
-                Destroy(gameObject);
+                gameObject.SetActive(false);
             }
             else
             {
