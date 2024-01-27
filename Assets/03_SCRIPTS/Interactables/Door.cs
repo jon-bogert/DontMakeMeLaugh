702e55fcaeefaf728b274d943672fd451c1a6283
jon-bogert <jonbogert@gmail.com>
2024-01-26 17:26:42 -0800

    Kill More enemies message

--- Assets/03_SCRIPTS/Interactables/Door.cs
+++ Assets/03_SCRIPTS/Interactables/Door.cs
@@ -5,6 +5,8 @@ public class Door : MonoBehaviour, IInteractable
     [SerializeField] GameObject _particleEffectPrefab;
     [SerializeField] Health[] _requiredObjects = new Health[0];
 
+    KillEnemiesMsg _msg;
+
     int _count = 0;
 
     private void Start()
@@ -14,12 +16,16 @@ public class Door : MonoBehaviour, IInteractable
         {
             h.onDeath.AddListener(TrackDeath);
         }
+        _msg = FindObjectOfType<KillEnemiesMsg>();
     }
 
     public void Interact()
     {
         if (_count > 0)
+        {
+            _msg.Invoke();
             return;
+        }
 
         ParticleSystem ps = Instantiate(_particleEffectPrefab, transform.position, transform.rotation).GetComponent<ParticleSystem>();
         ps.Play();
