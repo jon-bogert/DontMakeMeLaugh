cd95d498d15b677e089d5304c9c16082c695049a
Jon Bogert <jonbogert@gmail.com>
2024-01-27 10:47:16 -0800

    Godmode by key

--- Assets/03_SCRIPTS/_Dev/GODMODE.cs
+++ Assets/03_SCRIPTS/_Dev/GODMODE.cs
@@ -1,11 +1,24 @@
+using System.Drawing;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class GODMODE : MonoBehaviour
 {
     static GODMODE instance;
-    [SerializeField] bool _godMode = false;
+    bool _godMode = false;
     public static bool isGodModeEnabled { get { return instance._godMode; } }
+
+    private void Update()
+    {
+        if (!Debug.isDebugBuild)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _godMode = !_godMode;
+            Debug.Log("<color=magenta>GODMODE: " + _godMode.ToString() + "</color>");
+        }
+    }
 
     private void Awake()
     {
