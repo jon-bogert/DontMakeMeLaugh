61152516b840b81ea5c536528134cf7127bfd656
Jon Bogert <jonbogert@gmail.com>
2024-01-27 13:16:52 -0800

    Update RetroEffects.cs

--- Assets/03_SCRIPTS/Systems/RetroEffects.cs
+++ Assets/03_SCRIPTS/Systems/RetroEffects.cs
@@ -2,9 +2,26 @@ using UnityEngine;
 
 public class RetroEffects : MonoBehaviour
 {
-    private void Awake()
+    bool _effectOn = true;
+    private void Start()
     {
         Screen.SetResolution(360, 240, true, 30);
         Application.targetFrameRate = 30;
+        QualitySettings.vSyncCount = 0;
+    }
+
+    public void Toggle()
+    {
+        _effectOn = !_effectOn;
+        if (_effectOn)
+        {
+            Screen.SetResolution(360, 240, true, 30);
+            Application.targetFrameRate = 30;
+            QualitySettings.vSyncCount = 0;
+            return;
+        }
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+        Application.targetFrameRate = -1;
+        QualitySettings.vSyncCount = 1;
     }
 }
